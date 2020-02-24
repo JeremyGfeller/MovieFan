7efc7d7437885320097885c030bdbc3608da26a7
Jérémy Gfeller <36196039+JeremyGfeller@users.noreply.github.com>
2020-02-24 08:58:24 +0100

    inital commit

---
+++ MovieFan/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieFan.Models
+{
+    public class Movie
+    {
+        private string title;
+        public Movie(string title)
+        {
+            this.title = title;
+        }
+
+    }
+}
