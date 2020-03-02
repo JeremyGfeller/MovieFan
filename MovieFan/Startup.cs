63c01dd525ff03c567782bfd368befe271291419
Jérémy Gfeller <36196039+JeremyGfeller@users.noreply.github.com>
2020-03-02 10:55:30 +0100

    Change the method for conecting to database

--- MovieFan/Startup.cs
+++ MovieFan/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MovieFan.Models;
 
 namespace MovieFan
 {
@@ -24,6 +26,7 @@ namespace MovieFan
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<moviefanContext>(options => options.UseSqlServer(Configuration.GetSection("ConnectionStrings").GetSection("DefaultConnection").Value));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
